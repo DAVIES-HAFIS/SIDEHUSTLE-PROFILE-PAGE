9def4cf262e6b8712ddec2dd67157806fe720ed2
hafis davies <hafis.davies01@gmail.com>
2021-11-11 16:06:10 +0100

    my profile task

--- lib/main.dart
+++ lib/main.dart
@@ -23,88 +23,131 @@ class ProfilePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      
+      appBar: AppBar(
+        backgroundColor: Colors.indigo,
+        title: Text("My Profile", style: TextStyle(color: Colors.white, fontSize: 20us, fontWeight: FontWeight.bold),),
+        centerTitle: true,
+      ),
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Container(
-            height: 700,
-            decoration: BoxDecoration(
-              color: Colors.orange,
-              borderRadius: BorderRadius.circular(20),
-              border: Border.all(color: Colors.pink,width: 1),
-            ),
-            padding: EdgeInsets.all(20.0),
-            margin: EdgeInsets.all(20.0),
-            child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-              CircleAvatar(
-                backgroundImage:
-                AssetImage("assets/images/accountimg.png"),
-                radius: 20,
-              ),
-              Text(
-                'group2@sidehustle.com',
-                style: TextStyle(color: Colors.white),
-              ),
-                Icon(Icons.message, color: Colors.white,),
-              ],
-          ),
-                  SizedBox(
-                    height: 100,
-                  ),
-                  Container(
-                    height: 100,
-                    width: 100,
-                    child: Image.asset("assets/images/accountimg.png"),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Text("GROUP TWO SIDE HUSTLE \n  MOBILE DEV GROUP", style: TextStyle(color: Colors.black , fontWeight: FontWeight.bold, fontSize: 14),textAlign: TextAlign.center,),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Text("TEAM LEAD : SAMUEL",style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14, color: Colors.brown),),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Text("Email Address: group2@sidehustle.com ",style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14, color: Colors.black45),),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Text("Click on links below for more information on our social media pages",style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14, color: Colors.white),textAlign: TextAlign.center,),
-                  Container(
-                    height: 50,
-                    width: 100,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Container(
-                          decoration: BoxDecoration(
-                            color: Colors.white70,
-                          shape: BoxShape.circle),
-                          padding: EdgeInsets.all(10) ,
-                            child: Image.asset("assets/images/google.png",width: 15, height: 30,)),
-                        Container(
-                            decoration: BoxDecoration(
-                                color: Colors.white70,
-                                shape: BoxShape.circle),
-                            padding: EdgeInsets.all(10) ,
-                            child: Image.asset("assets/images/facebook.png",width: 15, height: 30,color: Colors.blue,)),
-                      ],
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 30),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children:[
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                  border: Border.all(color: Colors.grey, width: 1),
+                ),
+                padding: EdgeInsets.all(12.0),
+                height: 120,
+                width: double.infinity,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    CircleAvatar(
+                      radius: 30,
+                      child: Image.asset("assets/images/accountimg.png"),
                     ),
-                  ),
-                ],
+                    SizedBox(width: 30,),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Davies Hafis", style: TextStyle(fontSize: 14,fontWeight: FontWeight.w500),),
+                        SizedBox(height: 10,),
+                        Text("tejnik2007@yahoo.co.uk", style: TextStyle(fontSize: 12,),),
+                        Text("University of Lagos", style: TextStyle(fontSize: 12,),),
+                      ],
+                    )
+                  ],
+                ),
               ),
-            ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 15.0),
+                child: Text("General", style: TextStyle(fontSize: 14,fontWeight: FontWeight.w500),),
+              ),
+              Padding(
+                padding:EdgeInsets.only(bottom: 15.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0),
+                    border: Border.all(color: Colors.black26, width: 1),
+                  ),
+                  child: ListTile(
+                    title: Text("Edit profile" ,style: TextStyle(fontSize: 14,fontWeight: FontWeight.w400),),
+                    leading: Icon(Icons.person,color: Colors.indigo,size: 20,),
+                    trailing: Icon(Icons.arrow_forward_ios_rounded,color: Colors.grey,size: 20,),
+                  ),
+                ),
+              ),
+              Padding(
+                padding:EdgeInsets.only(bottom: 15.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0),
+                    border: Border.all(color: Colors.black26, width: 1),
+                  ),
+                  child: ListTile(
+                    title: Text("Badges" ,style: TextStyle(fontSize: 14,fontWeight: FontWeight.w400),),
+                    leading: Icon(Icons.badge,color: Colors.indigo,size: 20,),
+                    trailing: Icon(Icons.arrow_forward_ios_rounded,color: Colors.grey,size: 20,),
+                  ),
+                ),
+              ),
+              Padding(
+                padding:EdgeInsets.only(bottom: 15.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0),
+                    border: Border.all(color: Colors.black26, width: 1),
+                  ),
+                  child: ListTile(
+                    title: Text("Study Goal" ,style: TextStyle(fontSize: 14,fontWeight: FontWeight.w400),),
+                    leading: Icon(Icons.add_circle_outline_outlined,color: Colors.indigo,size: 20,),
+                    trailing: Icon(Icons.arrow_forward_ios_rounded,color: Colors.grey,size: 20,),
+                  ),
+                ),
+              ),
+              Padding(
+                padding:EdgeInsets.only(bottom: 15.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0),
+                    border: Border.all(color: Colors.black26, width: 1),
+                  ),
+                  child: ListTile(
+                    title: Text("Focus Mode" ,style: TextStyle(fontSize: 14,fontWeight: FontWeight.w400),),
+                    leading: Icon(Icons.notifications_off,color: Colors.indigo,size: 20,),
+                    trailing: Icon(Icons.arrow_forward_ios_rounded,color: Colors.grey,size: 20,),
+                  ),
+                ),
+              ),
+              Padding(
+                padding:EdgeInsets.only(bottom: 15.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0),
+                    border: Border.all(color: Colors.black26, width: 1),
+                  ),
+                  child: ListTile(
+                    title: Text("School Schedule" ,style: TextStyle(fontSize: 14,fontWeight: FontWeight.w400),),
+                    leading: Container(
+                        child: Icon(Icons.refresh_outlined,color: Colors.white,size: 15,),
+                        padding: EdgeInsets.all(3.0),
+                      decoration: BoxDecoration(
+                        color: Colors.indigo,
+                        shape: BoxShape.circle,
+                      ),
+
+                    ),
+                    trailing: Icon(Icons.arrow_forward_ios_rounded,color: Colors.grey,size: 20,),
+                  ),
+                ),
+              ),
+            ]
           ),
-        ),
+        )
       ),
     );
   }
